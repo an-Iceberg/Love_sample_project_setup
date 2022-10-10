606f18f932ac44e9d1478a93f30361b172448a8a
an-Iceberg <a.g.marosi@bluewin.ch>
2022-10-10 21:37:21 +0200

    first commit

---
+++ conf.lua
@@ -0,0 +1,12 @@
+function love.conf(game)
+  -- game.version = "11.4"
+  game.window.width = 800
+  game.window.height = 600
+  game.window.x = 300
+  game.window.y = 100
+  game.window.resizable = false
+  game.window.vsync = false
+  game.window.msaa = 0
+  game.window.title = "Sample Project Setup"
+  -- game.window.icon = "path/to/icon"
+end
---
+++ main.lua
@@ -0,0 +1,37 @@
+math.randomseed(os.time())
+
+-- Init
+function love.load()
+end
+
+-- Mouse input
+function love.mousepressed(x, y, button)
+end
+
+function love.mousemoved(x, y, delta_x, delta_y)
+end
+
+function love.wheelmoved(x, y)
+end
+
+-- Keybaord input
+function love.keypressed(key, scancode, is_repeat)
+end
+
+--[[
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
+end
+]]
+
+-- Update game state
+function love.update(delta_time)
+end
+
+-- Draw game to screen
+function love.draw()
+end
+
+-- Quit
+function love.quit()
+end
